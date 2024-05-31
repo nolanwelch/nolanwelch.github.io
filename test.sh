813e9881c464520712dc854a644434b7cced7270
Nolan Welch <nolanwelch@outlook.com>
2024-05-31 23:17:10 +0200

    Reload test server on save

--- test.sh
+++ test.sh
@@ -4,4 +4,4 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd "${SCRIPT_DIR}/docs"
 
 bundle install
-bundle exec jekyll serve
+bundle exec jekyll serve --livereload
